7e4a776f9e5b4f5acaf639c0dea58e7fb7a7cddb
Capwell Murimi <capwellmurimi@gmail.com>
2023-08-02 15:39:37 +0300

    update

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ window.onload   =   ()  =>  {
     const   root    =   ReactDOM.createRoot(rootElement) 
 
     const ints  =   [1, 2, 3]
-    const   anotherArray    =   []
+    const   anotherArray=[]
 
-    anotherArray.push(React.createElement("li", {key:ints[0]}, ints[0]))
+    anotherArray.push(React.createElement("li", {key:ints[0]},ints[0]))
+    anotherArray.push(React.createElement("li", {key:ints[1]},ints[1]))
+    anotherArray.push(React.createElement("li", {key:ints[2]},ints[2]))
 
     root.render(anotherArray)
 }
